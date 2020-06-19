f00a20053e13f598964f1487c03747c86f20dce9
meeks21 <anthony.meeks3@gmail.com>
2020-06-19 13:58:58 -0700

    jquery added

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$( function() {
+    $( "#menu" ).menu();
+  } );
+
+
+  //Tabs widget
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+  $( function() {
+    $( "input" ).checkboxradio();
+  } );
