9239cfc3951aef1dbd5e9904c2227485de4adb78
meeks21 <anthony.meeks3@gmail.com>
2020-06-19 14:20:34 -0700

    adds more javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 
+//menu widget
 $( function() {
     $( "#menu" ).menu();
   } );
 
 
-  //Tabs widget
+//Tabs widget
   $( function() {
     $( "#tabs" ).tabs();
   } );
 
-
+//Datepicker Widget
   $( function() {
     $( "#datepicker" ).datepicker();
   } );
-
+//checkbox radio widget
   $( function() {
     $( "input" ).checkboxradio();
   } );
